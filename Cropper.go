0337d8aff397898f2175f99f4daed2cd0d3fc256
Joe <joe@fabisevi.ch>
2013-08-17 01:04:38 -0400

    Cropper now returns images instead of saving them to the file syste

--- Cropper.go
+++ Cropper.go
@@ -1,7 +1,6 @@
 package Stitcher
 
 import (
-	"fmt"
 	"github.com/disintegration/imaging"
 	"image"
 )
@@ -10,28 +9,28 @@ const (
 	squareSide = 640
 )
 
-func CropCenter(sourceImage image.Image) {
+func CropCenter(sourceImage image.Image) image.Image {
 	x0 := (sourceImage.Bounds().Size().X - squareSide) / 2
 	y0 := (sourceImage.Bounds().Size().Y - squareSide) / 2
 
-	croppedPath := fmt.Sprintf("Cropped-%s", filePath)
-	cropTo640Squared(sourceImage, croppedPath, x0, y0)
+	croppedImage := cropTo640Squared(sourceImage, x0, y0)
+	return croppedImage
 }
 
-func CropFromTop(sourceImage image.Image) {
+func CropFromTop(sourceImage image.Image) image.Image {
 	x0 := 0
 	y0 := 0
 
-	croppedPath := fmt.Sprintf("Cropped-%s", filePath)
-	cropTo640Squared(sourceImage, croppedPath, x0, y0)
+	croppedImage := cropTo640Squared(sourceImage, x0, y0)
+	return croppedImage
 }
 
-func CropFromBottom(sourceImage image.Image) {
+func CropFromBottom(sourceImage image.Image) image.Image {
 	x0 := sourceImage.Bounds().Size().X - squareSide
 	y0 := sourceImage.Bounds().Size().Y - squareSide
 
-	croppedPath := fmt.Sprintf("Cropped-%s", filePath)
-	cropTo640Squared(sourceImage, croppedPath, x0, y0)
+	croppedImage := cropTo640Squared(sourceImage, x0, y0)
+	return croppedImage
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -40,13 +39,14 @@ func CropFromBottom(sourceImage image.Image) {
 func cropImage(sourceImage image.Image, x0, y0, x1, y1 int) image.Image {
 	currImage := imaging.Clone(sourceImage)
 	croppedImage := imaging.Crop(currImage, image.Rect(x0, y0, x1, y1))
+
 	return croppedImage
 }
 
-func cropTo640Squared(sourceImage image.Image, outputFileName string, x0, y0 int) {
+func cropTo640Squared(sourceImage image.Image, x0, y0 int) image.Image {
 	x1 := squareSide + x0
 	y1 := squareSide + y0
 
 	finalImage := cropImage(sourceImage, x0, y0, x1, y1)
-	imaging.Save(finalImage, outputFileName)
+	return finalImage
 }
