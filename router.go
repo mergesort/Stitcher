264e727e30bee800907d0191e57f524e1f9fcb7e
Joe <joe@fabisevi.ch>
2013-08-31 23:12:40 -0400

    Currently crashes on trying to process the image, which is not downloading correctly

--- router.go
+++ router.go
@@ -4,8 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/mergesort/Response"
+	"image"
+	"image/jpeg"
 	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -41,8 +44,8 @@ func StitcherHandler(rw http.ResponseWriter, req *http.Request) {
 					currURL := unmarshalledRequest.ImageURLs[i]
 					fmt.Println(i, currURL)
 				}
-				response := responseJSON.Response{"status": "200", "description": "Success!", "url": "http://app.net.com/candy"}
-				sendResponse(rw, response)
+
+				downloadImages(unmarshalledRequest.ImageURLs)
 			}
 		}
 	} else {
@@ -56,4 +59,53 @@ func StitcherHandler(rw http.ResponseWriter, req *http.Request) {
 
 func sendResponse(rw http.ResponseWriter, response responseJSON.Response) {
 	fmt.Fprint(rw, response)
+}
+
+func sendImageToS3(image image.Image) string {
+	return ""
+}
+
+func downloadImages(imageURLs []string) {
+	var group sync.WaitGroup
+	images := make([]image.Image, len(imageURLs))
+
+	for _, imageURL := range imageURLs {
+		group.Add(1)
+		go func() {
+			defer group.Done()
+			downloadImage(imageURL)
+		}()
+	}
+	group.Wait()
+
+	resultingImage := StitchImages(images)
+	sendImageToS3(resultingImage)
+
+	//create slice of images
+	//download each image
+	//when each one is there, stitch them, then call sendImageToS3
+	// DownloadImage(url)
+	//when you get a response from s3, call sendResponse with the URL
+
+}
+
+func downloadImage(imageURL string) image.Image {
+	response, fetchError := http.Get(imageURL)
+	if fetchError != nil {
+		fmt.Println("An error occurred downloading ", imageURL, fetchError)
+		return nil
+	} else {
+		defer response.Body.Close()
+
+		image, decodeError := jpeg.Decode(response.Body)
+
+		if decodeError != nil {
+			fmt.Println("An error occurred decoding ", imageURL, decodeError)
+			return nil
+		} else {
+			fmt.Println(response)
+			fmt.Println(response.Body)
+			return image
+		}
+	}
 }
